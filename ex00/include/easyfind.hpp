704645a367b4cbefd1a58702177223b196e50fac
takashi247 <takashi247@gmail.com>
2022-02-19 07:37:32 +0900

    add const version of easyfind and test cases

--- ex00/include/easyfind.hpp
+++ ex00/include/easyfind.hpp
@@ -2,8 +2,35 @@
 #define EASYFIND_HPP
 
 template <typename T>
+typename T::iterator easyfind(T& cont, int num) {
+  return std::find(cont.begin(), cont.end(), num);
+}
+
+template <typename T>
 typename T::const_iterator easyfind(const T& cont, int num) {
   return std::find(cont.begin(), cont.end(), num);
+}
+
+template <typename T>
+void testEasyFind(T& cont, int num) {
+  typename T::iterator it = easyfind(cont, num);
+  typename T::iterator it_end = cont.end();
+  if (it == it_end) {
+    std::cout << "not found" << std::endl;
+  } else {
+    std::cout << *it << std::endl;
+  }
+}
+
+template <typename T>
+void testEasyFind(const T& cont, int num) {
+  typename T::const_iterator it = easyfind(cont, num);
+  typename T::const_iterator it_end = cont.end();
+  if (it == it_end) {
+    std::cout << "not found" << std::endl;
+  } else {
+    std::cout << *it << std::endl;
+  }
 }
 
 #endif // EASYFIND_HPP
--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -1,27 +1,47 @@
 #include <iostream>
 #include <list>
 #include <vector>
+#include <deque>
+#include <set>
+#include <map>
+
 #include "easyfind.hpp"
 
 int main() {
+  int arr[] = {0, 1, 2, 3, 4};
   {
-    const int table[] = {0, 1, 2, 3, 4};
-    std::list<int> lst(table, table + 5);
-    std::list<int>::const_iterator it = easyfind(lst, 5);
-    if (it == lst.end()) {
-      std::cout << "not found" << std::endl;
-    } else {
-      std::cout << *it << std::endl;
-    }
+    std::cout << "\n---lst test---\n" << std::endl;
+    std::list<int> cont(arr, arr + 5);
+    std::list<int>::iterator it = cont.begin();
+    *it = 42;
+    testEasyFind(cont, 3);
+    testEasyFind(cont, 42);
   }
   {
-    const int table2[] = {0, 1, 2, 3, 4};
-    std::vector<int> vec(table2, table2 + 5);
-    std::vector<int>::const_iterator it  = easyfind(vec, 2);
-    if (it == vec.end()) {
-      std::cout << "not found" << std::endl;
-    } else {
-      std::cout << *it << std::endl;
-    }
+    std::cout << "\n---const lst test---\n" << std::endl;
+    const std::list<int> cont(arr, arr + 5);
+    // std::list<int>::const_iterator it = cont.begin();
+    // *it = 42;
+    testEasyFind(cont, 3);
+    testEasyFind(cont, 42);
   }
+  {
+    std::cout << "\n---deque test---\n" << std::endl;
+    std::deque<int> cont(arr, arr + 5);
+    testEasyFind(cont, 3);
+    testEasyFind(cont, 42);
+  }
+  {
+    std::cout << "\n---set test---\n" << std::endl;
+    std::set<int> cont(arr, arr + 5);
+    testEasyFind(cont, 3);
+    testEasyFind(cont, 42);
+  }
+  {
+    std::cout << "\n---multiset test---\n" << std::endl;
+    std::multiset<int> cont(arr, arr + 5);
+    testEasyFind(cont, 3);
+    testEasyFind(cont, 42);
+  }
+  std::cout << std::endl;
 }
