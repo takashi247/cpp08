8aeee65dd55649bd2eb49b14f8dce5fd828f3b9a
takashi247 <takashi247@gmail.com>
2022-02-18 12:49:39 +0900

    add some iterator operations

--- ex02/include/mutantstack.hpp
+++ ex02/include/mutantstack.hpp
@@ -2,11 +2,46 @@
 #define MUTANTSTACK_HPP
 
 #include <stack>
-#include <deque>
+#include <iterator>
 
 template <typename T>
 class MutantStack : public std::stack<T> {
  public:
+  class iterator : public std::iterator<std::forward_iterator_tag, T> {
+   private:
+    size_t index_;
+    MutantStack* stack_;
+    iterator() : index_(SIZE_MAX), stack_(NULL) {};
+    iterator(MutantStack* stack, size_t index) : index_(index), stack_(stack) {}
+
+   public:
+    iterator(const iterator& other) { *this = other; };
+    iterator& operator=(const iterator &other) {
+      if (this != &other) {
+        index_ = other.index_;
+        stack_ = other.stack_;
+      }
+      return *this;
+    }
+    iterator& operator++() {
+      ++index_;
+      return *this;
+    };
+    iterator operator++(int) {
+      iterator tmp = *this;
+      ++(*this);
+      return tmp;
+    };
+    int& operator*() {
+      // neee to implement
+    };
+    bool operator==(const iterator& iterator) {
+      // need to implement
+    };
+    bool operator!=(const iterator& iterator) {
+      // need to implement
+    };
+  };
   MutantStack(){};
   MutantStack(const MutantStack &other){ *this = other; };
   MutantStack &operator=(const MutantStack &other){
