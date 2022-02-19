f5ff8f12092a108529ee845231321b98524571bd
takashi247 <takashi247@gmail.com>
2022-02-19 11:44:28 +0900

    add necessary headers in easyfind header

--- ex00/include/easyfind.hpp
+++ ex00/include/easyfind.hpp
@@ -1,6 +1,9 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <algorithm>
+#include <iostream>
+
 template <typename T>
 typename T::iterator easyfind(T& cont, int num) {
   return std::find(cont.begin(), cont.end(), num);
